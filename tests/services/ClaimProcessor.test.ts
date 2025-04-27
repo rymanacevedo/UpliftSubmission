d6af87d8126a1c1febfc3760571cd2c5d6e28a53
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-04-27 13:58:37 -0600

    updated test case with reason code

--- tests/services/ClaimProcessor.test.ts
+++ tests/services/ClaimProcessor.test.ts
@@ -1,4 +1,5 @@
 import { Policy } from '../../src/models/Policy';
+import { ReasonCode } from '../../src/models/ReasonCode';
 import { exampleClaim } from './../../data/claim';
 import { ClaimProcessor } from './../../src/services/ClaimProcessor';
 
@@ -46,7 +47,7 @@ it('should payout the amount claimed minus deductible', async() => {
 }
 );
 
-it('should return zero if payout is negative or zero', async() => {
+it('should return zero if payout is negative', async() => {
     const processor = new ClaimProcessor();
     const validPolicyWithLargeDeductible: Policy = {
         policyId: 'POL123',
@@ -56,9 +57,11 @@ it('should return zero if payout is negative or zero', async() => {
         coverageLimit: 10000,
         coveredIncidents: ['accident', 'fire'],
     }
-    const {payout} = processor.evaluateClaim(exampleClaim, validPolicyWithLargeDeductible);
+    const {payout, reasonCode} = processor.evaluateClaim(exampleClaim, validPolicyWithLargeDeductible);
     expect(payout).toBe(0);
+    expect(reasonCode).toBe(ReasonCode.ZERO_PAYOUT);
 });
+
 // it('should approve a claim', async() => {
 //     const processor = new ClaimProcessor();
 //     processor.evaluateClaim({}, []);
