4926bc6703f7b22f4d37caafd9c8c422ae44bcc2
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-04-27 14:33:27 -0600

    added helper funciton to create policy

---
+++ tests/helpers/createPolicy.ts
@@ -0,0 +1,13 @@
+import { Policy } from "../../src/models/Policy";
+
+const createPolicy = (overrides?: Partial<Policy>): Policy => {
+    const basePolicy: Policy = {
+      policyId: 'POL-TEST',
+      startDate: new Date('2022-01-01'),
+      endDate: new Date('2025-12-31'),
+      deductible: 500,
+      coverageLimit: 10000,
+      coveredIncidents: ['accident', 'fire', 'theft', 'water damage'],
+    };
+    return { ...basePolicy, ...overrides };
+  };
