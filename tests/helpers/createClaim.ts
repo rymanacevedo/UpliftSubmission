1cc54521c33fa4bf7438fc87da9b6238f50166f5
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-04-27 14:34:13 -0600

    added helper funciton to create claim

---
+++ tests/helpers/createClaim.ts
@@ -0,0 +1,11 @@
+import { Claim } from "../../src/models/Claim";
+
+const createClaim = (overrides?: Partial<Claim>): Claim => {
+    const baseClaim: Claim = {
+        policyId: 'POL-TEST',
+        incidentType: 'fire',
+        incidentDate: new Date('2023-06-15'),
+        amountClaimed: 3000,
+    };
+    return { ...baseClaim, ...overrides };
+  };
