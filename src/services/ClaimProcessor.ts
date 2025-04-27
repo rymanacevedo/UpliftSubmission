f48e55485200a579cbd0b28b04ddd85b5193a208
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-04-27 13:20:21 -0600

    added code for evaluating an expired claim

---
+++ src/services/ClaimProcessor.ts
@@ -0,0 +1,28 @@
+import type { ClaimStatus } from './../models/ClaimStatus';
+import { Claim } from "../models/Claim";
+import { Policy } from "../models/Policy";
+
+
+export class ClaimProcessor {
+    private claimStatus: ClaimStatus;
+
+    constructor() {
+        this.claimStatus = 'pending';
+    }
+
+    public evaluateClaim(claim: Claim, policy: Policy) {
+        if(!policy) {
+            this.claimStatus = 'denied';
+            return;
+        }
+
+        if(policy.endDate < new Date()) {
+            this.claimStatus = 'denied';
+            return;
+        }   
+    }
+
+    public getClaimStatus(): ClaimStatus {
+        return this.claimStatus;
+    }
+}
